c9b9ec6e3baa9e949f436c04d1938ce7f1fe6029
soyunmate <gm.barahona.dev@gmail.com>
2024-08-22 19:50:04 -0400

    add selenium integration

---
+++ src/test/java/cl/talentodigital/appmanageevents/SeleniumTests.java
@@ -0,0 +1,40 @@
+package cl.talentodigital.appmanageevents;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SeleniumTests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+        // Conectar al contenedor de Selenium
+        driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+    }
+
+    @Test
+    public void testGoogleTitle() {
+        // Abrir Google y verificar el título
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        assertEquals("Google", title);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}

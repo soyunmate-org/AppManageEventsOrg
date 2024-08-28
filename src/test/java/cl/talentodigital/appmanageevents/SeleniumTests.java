7320da4e3304ee68f6498dc7d063a5e98aec21bd
soyunmate <gm.barahona.dev@gmail.com>
2024-08-27 22:05:52 -0400

    test commit 2

--- src/test/java/cl/talentodigital/appmanageevents/SeleniumTests.java
+++ src/test/java/cl/talentodigital/appmanageevents/SeleniumTests.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         WebDriver driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
         driver.get("https://www.google.com");
 
-        System.out.println("test");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.titleIs("Google"));

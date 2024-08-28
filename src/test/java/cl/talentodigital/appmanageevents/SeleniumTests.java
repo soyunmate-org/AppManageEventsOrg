382edab875ca421327bd6218a93384a864a77854
soyunmate <gm.barahona.dev@gmail.com>
2024-08-27 20:59:01 -0400

    fix firefox and edge dependencies

--- src/test/java/cl/talentodigital/appmanageevents/SeleniumTests.java
+++ src/test/java/cl/talentodigital/appmanageevents/SeleniumTests.java
@@ -1,10 +1,12 @@
-package test.java.cl.talentodigital.appmanageevents;
+package cl.talentodigital.appmanageevents;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -29,6 +31,26 @@ public class SeleniumTests {
         driver.get("https://www.google.com");
         String title = driver.getTitle();
         assertEquals("Google", title);
+    }
+
+    @Test
+    public void testFirefox() throws MalformedURLException {
+        FirefoxOptions options = new FirefoxOptions();
+        WebDriver driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        assertEquals("Google", title);
+        driver.quit();
+    }
+
+    @Test
+    public void testEdge() throws MalformedURLException {
+        EdgeOptions options = new EdgeOptions();
+        WebDriver driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        assertEquals("Google", title);
+        driver.quit();
     }
 
     @AfterEach
